11d0e20783320d92fea350c52668bfd1653b9bfa
Velin <vtklisurov@gmail.com>
2019-07-29 14:18:41 +0300

    Make city filter depend on country

--- vtklisurov-wordpress-map-integration/outputXML.php
+++ vtklisurov-wordpress-map-integration/outputXML.php
@@ -1,38 +1,42 @@
-    <?php
-       $dom = new DOMDocument("1.0");
-       $node = $dom->createElement("markers");
-       $parnode = $dom->appendChild($node);
+<?php
+   $dom = new DOMDocument("1.0");
+   $node = $dom->createElement("markers");
+   $parnode = $dom->appendChild($node);
 
-       $user='velin';
-       $pass='9810017583';
-       $host='localhost';
-       $db='wp_task';
+   $user='velin';
+   $pass='9810017583';
+   $host='localhost';
+   $db='wp_task';
 
-       $dbcon = new mysqli($host,$user,$pass,$db);
-       $sqlget = "select id, name, latitude, longitude, country from map_coords where id>0";
-       if($_POST['city_country']!='') $sqlget .= " and country ='" . $_POST['city_country'] . "'";
-       if($_POST['city_name']!='') $sqlget .= " and name ='" . $_POST['city_name'] . "'";
-       if($_POST['city_timezone']!='') $sqlget .= " and timezone = '" . $_POST['city_timezone'] . "'";
-       if($_POST['city_type']!='ANY') $sqlget .= " and feature_code ='" . $_POST['city_type'] . "'";
-       $lat1 = $_POST['city_lat1'];
-       $lat2 = $_POST['city_lat2'];
-       $lng1 = $_POST['city_lng1'];
-       $lng2 = $_POST['city_lng2'];
-       if($lat1!='' && $lat2!='' && $lng1!='' && $lng2!='') $sqlget .=" and latitude >" . min($lat1,$lat2) . " and latitude <" . max($lat1,$lat2) . " and longitude >" . min($lng1,$lng2) . " and longitude <" . max($lng1,$lng2);
-       $sqlget .= ";";
-       $sqldata= $dbcon->query($sqlget);
+   $dbcon = new mysqli($host,$user,$pass,$db);
+   $sqlget = "select id, name, latitude, longitude, country from map_coords where id>0";
+   if($_POST['city_country']!='') $sqlget .= " and country ='" . $_POST['city_country'] . "'";
+   if($_POST['city_name']!='') $sqlget .= " and name ='" . $_POST['city_name'] . "'";
+   if($_POST['city_timezone']!='') $sqlget .= " and timezone = '" . $_POST['city_timezone'] . "'";
+   if($_POST['city_type']!='ANY') $sqlget .= " and feature_code ='" . $_POST['city_type'] . "'";
+   $lat1 = $_POST['city_lat1'];
+   $lat2 = $_POST['city_lat2'];
+   $lng1 = $_POST['city_lng1'];
+   $lng2 = $_POST['city_lng2'];
+   $lat1 = str_replace(',','.',$lat1);
+   $lat2 = str_replace(',','.',$lat2);
+   $lng1 = str_replace(',','.',$lng1);
+   $lng2 = str_replace(',','.',$lng2);
+   if($lat1!='' && $lat2!='' && $lng1!='' && $lng2!='') $sqlget .=" and latitude >" . min($lat1,$lat2) . " and latitude <" . max($lat1,$lat2) . " and longitude >" . min($lng1,$lng2) . " and longitude <" . max($lng1,$lng2);
+   $sqlget .= "order by name;";
+   $sqldata= $dbcon->query($sqlget);
 
-       header("Content-type: text/xml");
-       while ($row = $sqldata->fetch_assoc())
-       {
-          $node = $dom->createElement("marker");
-          $newnode = $parnode->appendChild($node);
-          $newnode->setAttribute("id",$row['id']);
-          $newnode->setAttribute("name",$row['name']);
-          $newnode->setAttribute("lat", $row['latitude']);
-          $newnode->setAttribute("lng", $row['longitude']);
-          $newnode->setAttribute("country", $row['country']);
-       }
-       ob_clean();
-       echo $dom->saveXML();
-    ?>
+   header("Content-type: text/xml");
+   while ($row = $sqldata->fetch_assoc())
+   {
+      $node = $dom->createElement("marker");
+      $newnode = $parnode->appendChild($node);
+      $newnode->setAttribute("id",$row['id']);
+      $newnode->setAttribute("name",$row['name']);
+      $newnode->setAttribute("lat", $row['latitude']);
+      $newnode->setAttribute("lng", $row['longitude']);
+      $newnode->setAttribute("country", $row['country']);
+   }
+   ob_clean();
+   echo $dom->saveXML();
+?>
--- vtklisurov-wordpress-map-integration/populate_menu.php
+++ vtklisurov-wordpress-map-integration/populate_menu.php
@@ -1,88 +1,86 @@
 <?php
 
-$user='velin';
-$pass='9810017583';
-$host='localhost';
-$db='wp_task';
+  $user='velin';
+  $pass='9810017583';
+  $host='localhost';
+  $db='wp_task';
 
-$dbcon = new mysqli($host,$user,$pass,$db);
+  $dbcon = new mysqli($host,$user,$pass,$db);
 
-echo '<form name="filter" id="filter" action="/wp-content/plugins/map-integration/outputXML.php" method="GET"  target="dummyframe">
-  <div>
-    <label for="country">Country:</label>
-    <input type="text" list="city_country" name="city_country" autocomplete="off"/>
-    <datalist id="city_country">';
-    $sqlget = "select distinct country from map_coords order by country";
-    $sqldata= $dbcon->query($sqlget);
-    while ($row = $sqldata->fetch_assoc())
-    {
-      echo '<option value="'. $row['country'] . '"> ' . $row['country'] . ' </option>';
-    }
+  echo '<form name="filter" id="filter" action="/wp-content/plugins/map-integration/outputXML.php" method="GET"  target="dummyframe">
+    <div>
+      <label for="country">Country:</label>
+      <input type="text" list="city_country" name="city_country" id="country" autocomplete="off"/>
+      <datalist id="city_country">';
+      $sqlget = "select distinct country from map_coords order by country";
+      $sqldata= $dbcon->query($sqlget);
+      while ($row = $sqldata->fetch_assoc())
+      {
+        echo '<option value="'. $row['country'] . '"> ' . $row['country'] . ' </option>';
+      }
 
-echo '</datalist>
-  </div>
-  <div>
-    <label for="name">City:</label>
-    <input type="text" id="name" name="city_name" autocomplete="off">
-  </div>
-  <div>
-    <label for="timezone">Timezone:</label>
-    <input type="text" list="city_timezone" name="city_timezone" autocomplete="off"/>
-    <datalist id="city_timezone">
-    <option value=""></option>';
-    $sqlget = "select distinct timezone from map_coords order by timezone";
-    $sqldata= $dbcon->query($sqlget);
-    while ($row = $sqldata->fetch_assoc())
-    {
-      echo '<option value="'. $row['timezone'] . '"> ' . $row['timezone'] . ' </option>';
-    }
-echo '</datalist>
-  </div>
-  <div>
-    <label for="type">Type:</label>
-    <select name="city_type">
-      <option value="ANY">Any</option>
-      <option value="PPL">Populated place</option>
-      <option value="PPLA">Seat of 1st order admin. division</option>
-      <option value="PPLA2">Seat of 2nd order admin. division</option>
-      <option value="PPLA3">Seat of 3rd order admin. division</option>
-      <option value="PPLA4">Seat of 4th order admin. division</option>
-      <option value="PPLC">Capital</option>
-      <option value="PPLF">Farm village</option>
-      <option value="PPLG">Seat of gevornment entity</option>
-      <option value="PPLh">Historical populated place</option>
-      <option value="PPLL">Populated locality</option>
-      <option value="PPLQ">Abandoned populated place</option>
-      <option value="PPLR">Religious populated place</option>
-      <option value="PPLW">Destroyed populated place</option>
-      <option value="PPLX">Section of populated place</option>
-    </select>
-  </div>
-  <div>
-    <label for="lat1">First point latitude:</label>
-    <input type="number" step="any" id="lat1" name="city_lat1">
-  </div>
-  <div>
-    <label for="lng1">First point longitude:</label>
-    <input type="number" step="any" id="lng1" name="city_lng1">
-  </div>
-  <div>
-    <label for="lat2">Second point latitude:</label>
-    <input type="number" step="any" id="lat2" name="city_lat2">
-  </div>
-  <div>
-    <label for="lng2">Second point longitude:</label>
-    <input type="number" step="any" id="lng2" name="city_lng2">
-  </div>
-  <div class="button">
-  <button type="button" id="submit">Filter</button>
-  </div>
-  <div class="button">
-  <button type="reset" onclick="deleteMarkers()">Reset</button>
-  </div>
-  </form>'
-
-
-
-
+  echo '</datalist>
+    </div>
+    <div>
+      <label for="name">City:</label>
+      <input type="text" list="city_name" name="city_name" id="name" autocomplete="off"/>
+      <datalist id="city_name">
+      </datalist>
+    </div>
+    <div>
+      <label for="timezone">Timezone:</label>
+      <input type="text" list="city_timezone" name="city_timezone" autocomplete="off"/>
+      <datalist id="city_timezone">
+      <option value=""></option>';
+      $sqlget = "select distinct timezone from map_coords order by timezone";
+      $sqldata= $dbcon->query($sqlget);
+      while ($row = $sqldata->fetch_assoc())
+      {
+        echo '<option value="'. $row['timezone'] . '"> ' . $row['timezone'] . ' </option>';
+      }
+  echo '</datalist>
+    </div>
+    <div>
+      <label for="type">Type:</label>
+      <select name="city_type">
+        <option value="ANY">Any</option>
+        <option value="PPL">Populated place</option>
+        <option value="PPLA">Seat of 1st order admin. division</option>
+        <option value="PPLA2">Seat of 2nd order admin. division</option>
+        <option value="PPLA3">Seat of 3rd order admin. division</option>
+        <option value="PPLA4">Seat of 4th order admin. division</option>
+        <option value="PPLC">Capital</option>
+        <option value="PPLF">Farm village</option>
+        <option value="PPLG">Seat of gevornment entity</option>
+        <option value="PPLh">Historical populated place</option>
+        <option value="PPLL">Populated locality</option>
+        <option value="PPLQ">Abandoned populated place</option>
+        <option value="PPLR">Religious populated place</option>
+        <option value="PPLW">Destroyed populated place</option>
+        <option value="PPLX">Section of populated place</option>
+      </select>
+    </div>
+    <div>
+      <label for="lat1">First point latitude:</label>
+      <input type="text" id="lat1" name="city_lat1">
+    </div>
+    <div>
+      <label for="lng1">First point longitude:</label>
+      <input type="text" id="lng1" name="city_lng1">
+    </div>
+    <div>
+      <label for="lat2">Second point latitude:</label>
+      <input type="text" id="lat2" name="city_lat2">
+    </div>
+    <div>
+      <label for="lng2">Second point longitude:</label>
+      <input type="text" id="lng2" name="city_lng2">
+    </div>
+    <div class="button">
+    <button type="button" id="submit" onclick="validate()">Filter</button>
+    </div>
+    <div class="button">
+    <button type="reset" onclick="deleteMarkers()">Reset</button>
+    </div>
+    </form>'
 ?>
