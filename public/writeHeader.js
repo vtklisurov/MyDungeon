f576f052c3890a6cf0648ff7efeee56b370175f7
Yumit Yayya <yumityayya@gmail.com>
2021-01-10 13:45:36 +0200

    Added header to home page

---
+++ public/writeHeader.js
@@ -0,0 +1,15 @@
+document.write(`
+<div id = "header">
+  <div id = "siteName">
+    MyDungeon
+  </div>
+  <div id="headerBtns">
+      <div id="cartdiv">
+      </div>
+      <div id="logindiv">
+      </div>
+  </div>
+</div>
+`);
+{/* <button onclick= "location.href = cart;">Cart</button> */}
+{/* <button class=\"headerBtn\" onclick=\"location.href = '/login';\">Login</button> */}
