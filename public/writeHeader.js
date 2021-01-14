9d5d600a140f08afd3ad39dc3c90fc4524e7af0a
Yumit Yayya <yumityayya@gmail.com>
2021-01-14 20:21:17 +0200

    added logout button to the header

--- public/writeHeader.js
+++ public/writeHeader.js
@@ -11,5 +11,22 @@ document.write(`
   </div>
 </div>
 `);
+
+$(document).ready(function(){
+  $.post("islogged", function (status){
+    if (status.user!=undefined){
+      var divcontent = "<div id=\"cartdiv\" class=\"containerDiv\"><button class=\"headerBtn\" onclick=\"location.href = '/logout';\">Logout</button></div>";
+      document.getElementById("headerBtns").innerHTML = document.getElementById("headerBtns").innerHTML + divcontent;
+
+
+      // var divcontent = "<button class=\"headerBtn\" onclick=\"location.href = '/profile';\">Profile</button>";
+      // document.getElementById("logindiv").innerHTML = divcontent;
+      // divcontent = "<button class=\"headerBtn\" onclick=\"location.href = '/cart';\">Cart</button>";
+      // document.getElementById("cartdiv").innerHTML = divcontent;
+    }
+  });
+
+});
+
 {/* <button onclick= "location.href = cart;">Cart</button> */}
 {/* <button class=\"headerBtn\" onclick=\"location.href = '/login';\">Login</button> */}
